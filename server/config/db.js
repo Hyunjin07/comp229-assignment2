0f5be2ce46fd11ef2abb24a1050f790b47df6d37
Hyunjin07 <111410288+Hyunjin07@users.noreply.github.com>
2022-10-31 00:46:05 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //URI: "mongodb://127.0.0.1:27017/book_store",
+  URI:"mongodb+srv://newuser:HYUNjin0725@myfirstcluster.elvda2s.mongodb.net/user_collection?retryWrites=true&w=majority",
+};
